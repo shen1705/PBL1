f65332a751dd9ce16d43d42414b4df29cc7f51b5
shen1705 <102278572+shen1705@users.noreply.github.com>
2026-03-22 20:57:04 +0700

    loop split
    chia 2 vòng lặp thành 2 lệnh:
    phiên người dùng (sau khi login)
    phiên chạy ATM (nhập pass IT)

--- ATM_Application/Function/logics/atm_control.cpp
+++ ATM_Application/Function/logics/atm_control.cpp
@@ -10,17 +10,42 @@
 
 using namespace std;
 
+
+void RunUserSession(User* currentUser, int& system_running, SessionRecord*& sessionRec) 
+{
+    int user_status = 1; 
+    
+
+    while (user_status) 
+    {
+        drawUserBox(currentUser->accnum, currentUser->balance, currentUser->maxtrans);
+        string cmd;
+        cout << "ATM>";
+        cin >> cmd;
+        
+        handlecommand(cmd, system_running, *currentUser, user_status, sessionRec);
+        
+        // Kích hoạt khi user nhập "shutdown" từ bên trong tài khoản
+        if (system_running == 0) {
+            break; 
+        }
+    }
+    logoutannounce();
+}
+
+
 void runATM()
 {
-    // data prepare
-    UserList* accounts = nullptr;
+    UserList* accounts = nullptr; 
     if (!LoadData(accounts))
     {
         cout << "cant load data from file";
     }
-    SessionRecord *sessionrecord = nullptr;
-    int system_running = 1; // active status
-    struct User *currentUser = nullptr;
+    
+    SessionRecord *sessionRec = nullptr; 
+    int system_running = 1;
+    User *currentUser = nullptr;
+
 
     while (system_running)
     {
@@ -32,48 +57,40 @@ void runATM()
         if (!(cin >> menuOption))
         {
             cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
         if (menuOption == "1")
         {
             int LoggedIn = Login(accounts, currentUser);
             if (LoggedIn == 1 && currentUser != nullptr)
             {
-                int user_status = 1;
-                while (user_status)
-                {
-                    {
-                        drawUserBox(currentUser->accnum, currentUser->balance, currentUser->maxtrans);
-                        string cmd;
-                        cout << "ATM>";
-                        cin >> cmd;
-                        handlecommand(cmd, system_running, *currentUser, user_status, sessionrecord);
-                    }
-                }
-                logoutannounce();
-                
+              
+                RunUserSession(currentUser, system_running, sessionRec);
             }
         }
         else if (menuOption == "0")
         {
             if (ITauth())
             {
-                system_running = 0;
+                system_running = 0; 
             }
         }
-        else {cout<<"Invalid Option."<<endl;
+        else {
+            cout<<"Invalid Option."<<endl;
             delay(3);
         }
-        // Save data at the end of the session
     }
-    shutdownAnnounce();
-    Record(sessionrecord);
+    
     SaveData(accounts);
+    Record(sessionRec);
+    shutdownAnnounce();
+
     UserList* current = accounts;
     while (current != nullptr) {
-        FreeHistory(current->data); 
+        FreeHistory(current->data);
         UserList* temp = current;
         current = current->next;
         delete temp; 
