0383349a34a717dabe584f2f391887ce7c8a8075
shen1705 <102278572+shen1705@users.noreply.github.com>
2026-03-31 14:50:47 +0700

    Ngon cơm

--- ATM_Application/Function/utils/UI.cpp
+++ ATM_Application/Function/utils/UI.cpp
@@ -2,8 +2,8 @@
 #include <iomanip>
 #include <string>
 #include <limits>
-#include <thread>//bo?
-#include <chrono>//bo?
+#include <thread> 
+#include <chrono> 
 #include <cstdlib>
 #include "UI.h"
 
@@ -57,24 +57,20 @@ void History(ostream &os, int no, double amount, string type)
        << "|" << right << setw(col3) << amtStr << "|" << endl;
 }
 
-// 
 void TransactionRecordRow(ostream &os, int accnum, string typeStr, double amount)
 {
     os << "|" << left << setw(18) << accnum 
        << "|" << left << setw(10) << typeStr
        << "|" << right << setw(28) << fixed << setprecision(2) << amount << "|" << endl;
 }
-// Main
+
+// Main UI 
 
 void drawMenuBox()
 {
     clearScreen();
     drawTitle(cout, "WELCOME TO MY_ATM PROGRAM");
     drawDivider(cout, 60, '-');
-    const int width = 60;
-    cout << "| " << left << setw(width - 4) << "1. Login - Type 1 to Login" << " |" << endl;
-    cout << "| " << left << setw(width - 4) << "0. Exit - Type 0 to Exit (Permission required)" << " |" << endl;
-    cout << "+" << string(width - 2, '=') << "+" << endl;
 }
 
 void drawUserBox(int accnum, double balance, int maxtrans)
@@ -91,10 +87,10 @@ void drawUserBox(int accnum, double balance, int maxtrans)
     cout << "| " << left << setw(width - 4) << limStr << " |" << endl;
     cout << "|" << string(width - 2, '-') << "|" << endl;
 
-    cout << "| " << left << setw(width - 4) << "1. deposit  - Deposit money - Gửi tiền" << " |" << endl;
-    cout << "| " << left << setw(width - 4) << "2. withdraw - Withdraw money - Rút tiền" << " |" << endl;
-    cout << "| " << left << setw(width - 4) << "3. history  - Show transactions - Lịch sử giao dịch" << " |" << endl;
-    cout << "| " << left << setw(width - 4) << "4. exit     - Log out - Đăng xuất" << " |" << endl;
+    cout << "| " << left << setw(width - 4) << "1. Deposit money - Gui tien" << " |" << endl;
+    cout << "| " << left << setw(width - 4) << "2. Withdraw money - Rut tien" << " |" << endl;
+    cout << "| " << left << setw(width - 4) << "3. Show transactions - Lich su giao dich" << " |" << endl;
+    cout << "| " << left << setw(width - 4) << "4. Log out - Dang xuat" << " |" << endl;
     cout << "+" << string(width - 2, '=') << "+" << endl;
 }
 
@@ -107,13 +103,13 @@ void showMessageAndDelay()
 void logoutannounce()
 {
     cout << "Logging out..." << endl;
-    delay(3);
+    delay(2);
 }
 
 void loginfailedannounce()
 {
     cout << "Login Failed! Wrong PIN" << endl;
-    delay(3);
+    delay(2);
 }
 
 void shutdownAnnounce()
@@ -131,11 +127,11 @@ void shutdownAnnounce()
 
 void systemMenu()
 {
-     system("cls");
-        cout << "=================================" << endl;
-        cout << "         IT ADMIN MENU           " << endl;
-        cout << "=================================" << endl;
-        cout << "[1]. Start ATM Session (Launch UI)" << endl;
-        cout << "[0]. Shutdown Entire Hardware" << endl;
-        cout << "=================================" << endl;
+    clearScreen();
+    const int width = 60;
+    drawTitle(cout, "IT ADMIN MENU");
+    drawDivider(cout, width, '-');
+    cout << "| " << left << setw(width - 4) << "1. Start ATM Session (Launch UI)" << " |" << endl;
+    cout << "| " << left << setw(width - 4) << "0. Shutdown Entire Hardware" << " |" << endl;
+    cout << "+" << string(width - 2, '=') << "+" << endl;
 }
